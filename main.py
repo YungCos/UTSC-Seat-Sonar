e28016916a94ea66598cd9ece7d574421700e655
Cosmo48 <59401027+Cosmo48@users.noreply.github.com>
2022-08-18 12:33:22 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,104 @@
+import discord
+import settings
+import firebase_admin
+from firebase_admin import db
+from discord.ext import commands
+from discord.ext import tasks
+import ttb
+
+intents = discord.Intents.default()
+
+bot = commands.Bot(command_prefix="!", help_command=None, intents=intents)
+
+cred_obj = firebase_admin.credentials.Certificate(settings.CRED_OBJ)
+default_app = firebase_admin.initialize_app(cred_obj, {
+    'databaseURL': settings.URL
+    })
+
+
+@tasks.loop(minutes = 15)
+async def notify():
+    users = db.reference(f"/Users").get()
+
+    for user in users:
+
+        userObj = await bot.fetch_user(user)
+
+        if not userObj:
+            print(user, "not found")
+            continue
+
+        for course in users[user]['Tutorials']:
+
+            tuts = []
+
+            for tut in users[user]['Tutorials'][course]:
+
+                tuts.append(tut)
+
+            available_tuts = ttb.findCourse(course, tuts)
+
+            for i in available_tuts:
+
+                await userObj.send(f"<@{user}> {i} for {course} has been made available. If you get the seat, you can remove this course using: `!remove {course} {tut}`")
+
+
+
+
+
+
+
+
+
+@bot.event
+async def on_ready():
+    print("Bot is ready!")
+
+    await bot.change_presence(status=discord.Status.online, activity=discord.Game("Sniping Tutorials"))
+    await notify.start()
+
+
+@bot.command()
+async def help(ctx):
+    embed=discord.Embed(title="Commands", color=0x009dff)
+    embed.add_field(name="!watch [Course Code] [Tutorial Name]", value="Adds a tutorial to be watched. Note that the tutorial name needs to be exact (Ex. !watch CSCB36 TUT0001)", inline=True)
+    embed.add_field(name="!courses", value="View the tutorials that you are currently watching", inline=True)
+    embed.add_field(name="!remove [Course Code] [Tutorial Name]", value="Removes a tutorial from the watchlist. Note the format is the same as !watch", inline=True)
+    await ctx.send(embed=embed)
+
+
+@bot.command()
+async def watch(ctx, course, tut):
+
+
+    db.reference(f"/Users/{ctx.author.id}/Tutorials/{course}").update({tut: 0})
+    await ctx.channel.send("You are now watching {tut} for {course}")
+
+@bot.command()
+async def courses(ctx):
+
+    userData = db.reference(f"/Users/{ctx.author.id}/Tutorials").get()
+    embed=discord.Embed(title="Tutorials Being Watched", color=0x009dff)
+
+    for i in userData:
+
+        course_name = i
+        tuts = []
+
+        for j in userData[i]:
+
+            tuts.append(j)
+
+        embed.add_field(name=course_name, value="\n".join(tuts), inline=True)
+
+    await ctx.send(embed=embed)
+
+@bot.command()
+async def remove(ctx, course, tut):
+    db.reference(f"/Users/{ctx.author.id}/Tutorials/{course}").update({tut: None})
+    await ctx.channel.send(f"You are no longer watching {tut} for {course}")
+
+
+
+bot.run(settings.TOKEN)
+
---
+++ ttb.py
@@ -0,0 +1,63 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import settings
+
+
+def findCourse(course, tuts):
+
+	available_tuts = []
+
+
+	PATH = settings.DRIVER_PATH
+
+	driver = webdriver.Chrome(PATH)
+
+	driver.get('https://ttb.utoronto.ca/')
+
+	dropdown = WebDriverWait(driver, 10).until(
+	    EC.presence_of_element_located((By.ID, "division"))
+	)
+
+	dropdown.click()
+
+	utsc = WebDriverWait(driver, 1).until(
+	    EC.presence_of_element_located((By.ID, "division-option-5-University of Toronto Scarborough"))
+	)
+
+	utsc.click()
+
+	courseSearch = WebDriverWait(driver, 1).until(
+	    EC.presence_of_element_located((By.XPATH, '//*[@id="courseSearch"]/div/div[2]/div[1]/input'))
+	)
+
+	courseSearch.send_keys(course)
+
+	search = WebDriverWait(driver, 1).until(
+	    EC.presence_of_element_located((By.XPATH, "/html/body/app-root/main/app-dashboard/app-search/div/div[3]/button[1]"))
+	)
+
+	search.click()
+
+	tut_parent = WebDriverWait(driver, 2).until(
+	    EC.presence_of_element_located((By.XPATH, '/html/body/app-root/main/app-dashboard/div/app-result/div[2]/app-course/div[3]/div[2]'))
+	)
+
+	all_tuts = tut_parent.find_elements(By.XPATH, "*")
+
+	for course in all_tuts[1:]:
+
+		tut_name = course.find_element(By.XPATH, "./div/h5/span").text
+
+		if tut_name in tuts:
+
+			avail = course.find_element(By.XPATH, "./div/div[1]/div[4]/span").text.split(" ")[0]
+			if int(avail) > 0:
+				available_tuts.append(tut_name)
+
+
+	driver.close()
+
+	return available_tuts
